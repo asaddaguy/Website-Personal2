eeec6dee5698b21c767452fc002d0e47e2fc3feb
asaddaguy <mhmd.asadjr12@gmail.com>
2024-08-24 14:46:34 +0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById('opening').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }, 3000); // 3 detik
+};
